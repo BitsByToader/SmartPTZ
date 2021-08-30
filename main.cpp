7b5f5dd8181fbab5194e3b3df34136e5f86daea7
Ifrim Tudor <ifrimtudor@protonmail.com>
2021-08-30 22:52:29 +0300

    First Hello World of this project.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <QCoreApplication>
+#include <QtDebug>
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+
+    qDebug()<<"Hello World!"<<Qt::endl;
 
     return a.exec();
 }
